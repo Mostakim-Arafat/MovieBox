8ea4bf421f2cc1e61abcb8b38ed59206e67a9e17
Tithe-cast <simaakter089129@gmail.com>
2026-08-24 20:25:38 +0600

    Footer and integrate into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
+       <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black w-full">
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
@@ -65,5 +66,7 @@ export default function Home() {
         </div>
       </main>
     </div>
+     <Footer />
+     </div>
   );
 }
