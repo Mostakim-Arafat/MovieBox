7ff03b4c4c32f32f535011668f5694ee0959c0e3
Mostakim-Arafat <mostakim2005arafat@gmail.com>
2026-08-27 16:50:58 +0600

    add tranding and fix navbar toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import FaqSection from "@/Components/Homepage/FaqSection";
 import BannerSection from "@/Components/Homepage/BannerSection";
+import TrendingNow from "@/Components/Homepage/TreandingNow";
 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground transition-all duration-300">
        <BannerSection></BannerSection>
+       <TrendingNow></TrendingNow>
       <FaqSection />
     </div>
   );
