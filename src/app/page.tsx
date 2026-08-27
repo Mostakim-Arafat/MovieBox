79cc41481f5365504936dd3f25d91dd3bef799a7
Tithe-cast <simaakter089129@gmail.com>
2026-08-27 12:48:29 +0600

    "Added style for Banner Section"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import FaqSection from "@/Components/Homepage/FaqSection";
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground transition-all duration-300">
+       
       <FaqSection />
     </div>
   );
