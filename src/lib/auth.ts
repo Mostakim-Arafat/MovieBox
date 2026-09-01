3f431fc281e8b9bf009e519c31e0b1ae402521e7
Mostakim-Arafat <mostakim2005arafat@gmail.com>
2026-09-01 19:05:37 +0600

    change folder in mongodatabase of user

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,13 +3,16 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGO_URL as string);
-const db = client.db();
+const db = client.db("Moviebox");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
+  user : {
+    modelName : "users"
+  },
     emailAndPassword: { 
     enabled: true, 
   }, 
