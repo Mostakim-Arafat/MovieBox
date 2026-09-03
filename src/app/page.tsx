7a1524f767f1aba2106389a1d7cc1056ecc62762
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-09-03 12:31:26 +0600

    Add pricing plans and payment method sections to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import FaqSection from "@/Components/Homepage/FaqSection";
 import BannerSection from "@/Components/Homepage/BannerSection";
 import TrendingNow from "@/Components/Homepage/TreandingNow";
+import PricingSection from "@/Components/PricingSection";
+import PaymentSection from "@/Components/PaymentSection";
 
 
 export default function Home() {
@@ -11,6 +13,8 @@ export default function Home() {
        <BannerSection></BannerSection>
        <TrendingNow></TrendingNow>
       <FaqSection />
+      <PricingSection/>
+      <PaymentSection/>
     </div>
   );
 }
