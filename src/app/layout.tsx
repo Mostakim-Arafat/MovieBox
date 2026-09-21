fbbf1ab1b660fc6756f5e505f3590f47cb273dba
Mostakim-Arafat <mostakim2005arafat@gmail.com>
2026-09-21 16:38:00 +0600

    set admin permission

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes"
 import ClientLayout from "@/Components/Admin/Clientlayout";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" enableSystem defaultTheme="system" disableTransitionOnChange>
           <ClientLayout>
             {children}
+            <Toaster/>
           </ClientLayout>
         </ThemeProvider>
       </body>
